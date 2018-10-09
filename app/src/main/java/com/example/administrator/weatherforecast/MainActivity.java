3ec6e40abb5f3a57002167270446ad78a71f7336
xl2637036982 <2637036982@qq.com>
2018-10-09 17:08:06 +0800

    完成ViewPager

--- app/src/main/java/com/example/administrator/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/administrator/weatherforecast/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.administrator.weatherforecast;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ViewPager mViewPager;
+    ArrayList<Fragment> fragments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initfragment();
+        mViewPager=findViewById(R.id.view_pager);
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                if (position==1) {
+
+                  Weather1Fragment fragment = new Weather1Fragment();
+                   return fragment;
+                }else {
+
+                    Weather2Fragment fragment2 = new Weather2Fragment();
+                  return fragment2;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
+
+
     }
+
+    private void initfragment() {
+        fragments.add(new Weather1Fragment());
+
+          fragments.add(new Weather2Fragment());
+}
+
+
+
+
 }
